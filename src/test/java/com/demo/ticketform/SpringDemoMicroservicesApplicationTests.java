311630f1fda7500f3066bc0e46aca4e4038f6dba
Lonrot <angelschmal.1a@gmail.com>
2025-08-24 23:15:46 +0100

    Initial commit

---
+++ src/test/java/com/demo/ticketform/SpringDemoMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ticketform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
