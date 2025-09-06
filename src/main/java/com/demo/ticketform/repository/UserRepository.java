4a9165a75c539147c6322c102d989ec1579d4ade
Lonrot <angelschmal.1a@gmail.com>
2025-09-06 18:26:54 +0100

    Feat: Add UserRepository with email lookup method

---
+++ src/main/java/com/demo/ticketform/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.demo.ticketform.repository;
+
+import com.demo.ticketform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+
+}
